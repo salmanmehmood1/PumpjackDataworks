da12fbae18442e289697a59f26e6fa213646d50e
mustafainraza <74792750+mustafainraza@users.noreply.github.com>
2022-06-21 18:57:01 +0500

    slope_formula.py

---
+++ mustafain/Day02/slope_formula.py
@@ -0,0 +1,16 @@
+import math
+n1=input('Enter First Number : ')
+n2=input('Enter Second Number : ')
+n1 = float(n1)
+n2 = float(n2)
+def slope_formula(a,b):
+    c=math.pow(a+b,2)
+    return c
+
+print(slope_formula(n1,n2))
+
+
+
+
+
+

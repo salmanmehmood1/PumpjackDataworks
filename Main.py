cf2726e7bdee7606977e5618be41ff8fef7a5c68
Erum-Abdul-Latif <87967100+Erum-Abdul-Latif@users.noreply.github.com>
2022-06-21 18:10:47 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import cmath
 ######### Basit Code Part
 def slope_formula(x11,x22,y11,y22):
     gradient= (y22-y11)/(x22-x11)
@@ -139,8 +140,30 @@ def slopeformula1():
     y2=float(input("Enter the second ycoordinate:"))
     Output=slope_formula(x1,x2,y1,y2)
     print("The slope of the given formula is:",Output)
-def slopeformula2():
-    return "wednesday"
+    
+def quadraticformula():
+    a = float(input('Enter a: '))  
+    b = float(input('Enter b: '))  
+    c = float(input('Enter c: '))  
+ 
+    determinant = (b**2) - (4*a*c)  
+
+    if determinant > 0:
+        root1 = (-b-cmath.sqrt(d))/(2*a) 
+        root2 = (-b+cmath.sqrt(d))/(2*a) 
+
+        print('root1 = {0} and root2 = {1}'.format(root1,root2))
+
+    elif determinant == 0:
+        root1 = root2 = -b / (2 * a)
+        print('root1 = root2 =', root1)
+
+    else:
+        real = -b/(2*a)
+        imaginary = cmath.sqrt(-determinant)/(2*a)
+        print('root1 = {0}+{1}i'.format(real,imaginary))
+        print('root2 = {0}+{1}i'.format(real,imaginary))
+        
 def quadraticformula():
     return "thursday"
 def laplaceformula():

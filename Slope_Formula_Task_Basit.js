a9c89faae6918f533fbf922099c47132a4e88487
Erum-Abdul-Latif <87967100+Erum-Abdul-Latif@users.noreply.github.com>
2022-06-21 15:03:25 +0500

    Delete Slope_Formula_Task_Basit.js

--- Slope_Formula_Task_Basit.js
+++
@@ -1,7 +0,0 @@
-const prompt=require("prompt-sync")({fake_val:"OPTIONAL CONFIG VALUES HERE",});
- let x1=parseInt(prompt("Enter first xcoordinate:"));
- let x2=parseInt(prompt("Enter the second xcoordinate:"));
- let y1=parseInt(prompt("Enter the first ycoordinate:"));
-let y2=parseInt(prompt("Enter the second ycoordinate:"));
-let gradient= (y2-y1)/(x2-x1)
-console.log('The slope is ',gradient );

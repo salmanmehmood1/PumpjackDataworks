3db942c7e1b93e7e50bbbca8cb7c257ddc539237
Basit Abbas <100775319+BasitAbbas19@users.noreply.github.com>
2022-06-22 18:20:07 +0500

    Update Day1.js

--- Basit/Day1.js
+++ Basit/Day1.js
@@ -1,11 +1,14 @@
 const prompt=require("prompt-sync")({fake_val:"OPTIONAL CONFIG VALUES HERE",});
+function slope_formula(x11,x22,y11,y22){
+    let gradient= (y22-y11)/(x22-x11);
+    return gradient;
+}
+function slp(){
  let x1=parseInt(prompt("Enter first xcoordinate:"));
  let x2=parseInt(prompt("Enter the second xcoordinate:"));
  let y1=parseInt(prompt("Enter the first ycoordinate:"));
 let y2=parseInt(prompt("Enter the second ycoordinate:"));
-function slope_formula(x11,x22,y11,y22){
-    let gradient= (y2-y1)/(x2-x1);
-    return gradient;
-}
 let output=slope_formula(x1,x2,y1,y2);
 console.log('The slope of the given coordinates is ',output );
+}
+slp()

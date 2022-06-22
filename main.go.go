2154df317a9b5f83ec1d831009681cc6862ccb84
Erum-Abdul-Latif <87967100+Erum-Abdul-Latif@users.noreply.github.com>
2022-06-22 16:49:02 +0500

    Update main.go.go

--- main.go.go
+++ main.go.go
@@ -183,6 +183,32 @@ func distance() {
 }
 
 //anabia
+
+//erum
+func quadraticformula() {
+var a, b, c, root1, root2, imaginary, discriminant float64
+
+    fmt.Print("Enter the a, b, c of Quadratic equation = ")
+    fmt.Scanln(&a, &b, &c)
+
+    discriminant = (b * b) - (4 * a * c)
+
+    if discriminant > 0 {
+        root1 = (-b + math.Sqrt(discriminant)/(2*a))
+        root2 = (-b - math.Sqrt(discriminant)/(2*a))
+        fmt.Println("Two Distinct Real Roots Exist: root1 = ", root1, " and root2 = ", root2)
+    } else if discriminant == 0 {
+        root1 = -b / (2 * a)
+        root2 = -b / (2 * a)
+        fmt.Println("Two Equal and Real Roots Exist: root1 = ", root1, " and root2 = ", root2)
+    } else if discriminant < 0 {
+        root1 = -b / (2 * a)
+        root2 = -b / (2 * a)
+        imaginary = math.Sqrt(-discriminant) / (2 * a)
+        fmt.Println("Two Distinct Complex Roots Exist: root1 = ", root1, "+", imaginary, " and root2 = ", root2, "-", imaginary)
+    }
+}
+//erum
 func main() {
 
 fmt.Println("Enter your choice in number: ")
@@ -197,13 +223,9 @@ case 2:
 case 3:
     slope()
 case 4:
-    fmt.Println("Thursday")
+    quadraticformula()
 case 5:
     fmt.Println("Friday")
-case 6:
-    fmt.Println("Saturday")
-case 7:
-    fmt.Println("Sunday")
 default:
     fmt.Println("Invalid")
 }
